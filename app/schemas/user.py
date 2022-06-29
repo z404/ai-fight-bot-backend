eda047c69b638391c49a47270354c8c60e3e8b34
Vishruth Devan <vishruthdevan@gmail.com>
2022-06-29 21:19:33 +0530

    feat: add schemas for crud operations on user

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -7,3 +7,27 @@ class UserBase(BaseModel):
     is_active: Optional[bool] = True
     is_superuser: bool = False
     full_name: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    email: EmailStr
+    password: str
+
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class UserInDBBase(UserBase):
+    id: Optional[int] = None
+
+    class Config:
+        orm_mode = True
+
+
+class User(UserInDBBase):
+    pass
+
+
+class UserInDB(UserInDBBase):
+    hashed_password: str
