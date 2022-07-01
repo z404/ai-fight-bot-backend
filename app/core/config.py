368ee22cb7a376ac33f5fadcf330a5bf77315678
Vishruth Devan <vishruthdevan@gmail.com>
2022-07-01 23:32:27 +0530

    feat: add method to create access token

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,11 @@
 from pydantic import BaseSettings
+import secrets
 
 
 class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URL = "sqlite:///./app/app_db.db"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    SECRET_KEY: str = secrets.token_urlsafe(32)
 
 
 settings = Settings()
