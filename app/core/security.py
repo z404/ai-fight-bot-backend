c00d2b81bdfce2238aca0c43cb662e7d4c893f5a
Vishruth Devan <vishruthdevan@gmail.com>
2022-07-01 23:33:01 +0530

    feat: add methods to hash and verify passwords

--- app/core/security.py
+++ app/core/security.py
@@ -25,3 +25,11 @@ def create_access_token(
     encoded_jwt = jwt.encode(
         to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
