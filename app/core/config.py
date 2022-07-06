82b0d5efd10bbc8387cb69c8ecf4ce2ce19665a2
Vishruth Devan <vishruthdevan@gmail.com>
2022-07-06 16:09:18 +0530

    chore: configure router path

--- app/core/config.py
+++ app/core/config.py
@@ -3,9 +3,11 @@ import secrets
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL = "sqlite:///./app/app_db.db"
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./app_db.db"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SECRET_KEY: str = secrets.token_urlsafe(32)
+    USERS_OPEN_REGISTRATION: bool = True
+    API_V1_STR: str = "/api/v1"
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
 
+from api.api import api_router
+from core.config import settings
+
+
 app = FastAPI(
     title="AI Fight Bot",
 )
 
 
-@app.get("/")
+@app.get("/hello")
 async def root():
     return {"message": "AI Fight Bot"}
+
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
 if __name__ == "__main__":
